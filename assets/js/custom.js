ed628eca8b522539af80c23310b309f98d74463c
Saurabh <saurabhgupta6360@gmail.com>
2023-07-22 14:29:29 +0530

    cmt

---
+++ assets/js/custom.js
@@ -0,0 +1,109 @@
+// home slider
+
+$(".single-item").slick({
+  autoplay: true,
+  dots: false,
+  arrows: false,
+  speed: 200,
+  slidesToShow: 1,
+});
+$(".one-time").slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  speed: 200,
+  slidesToShow: 1,
+  autoplay: true,
+  adaptiveHeight: true,
+});
+
+// PR Release
+
+$(".five-item").slick({
+  dots: false,
+  autoplay: true,
+  infinite: true,
+  speed: 200,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+// blog
+
+$(".blog-item, .testimonial-item ").slick({
+  dots: false,
+  autoplay: true,
+  infinite: true,
+  speed: 200,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: false,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+// Faq
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
